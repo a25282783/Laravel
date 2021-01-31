7f19ee0776684811fd47f2dac9fd80ee80c092cc
boy <bobbyliou830228@gmail.com>
2021-01-31 23:38:17 +0800

    mail verify custom

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\User;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function (User $user, string $verificationUrl) {
+            return (new MailMessage)
+                ->subject(Lang::get('messages.VerifyEmailAddress'))
+                ->line(Lang::get('Please click the button below to verify your email address.'))
+                ->action(Lang::get('Verify Email Address'), $verificationUrl);
+        });
     }
 }
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'VerifyEmailAddress' => '驗證',
+];
