2129334a8566481792eb5995d4e0bcbcf7243058
boy <bobbyliou830228@gmail.com>
2021-01-31 21:48:04 +0800

    mail verify

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Jobs\ProcessPodcast;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,20 +14,19 @@ use Illuminate\Support\Facades\Http;
 | contains the "web" middleware group. Now create something great!
 |
  */
-
+Auth::routes(['verify' => true]);
 Route::get('/{id}', function ($id) {
     ProcessPodcast::dispatch($id)->delay(now()->addSeconds(1));
     // $response = Http::get('http://localhost/insert/6');
     return "done$id";
-})->where('id', '[0-9]+');;
+})->where('id', '[0-9]+');
 Route::get('/insert/{id}', function ($id) {
     sleep(3);
-    Redis::set('id',$id);
+    Redis::set('id', $id);
 });
 
 Route::get('/test', function () {
     return Redis::get('id');
 })->where('test', '[A-Za-z]+');
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
